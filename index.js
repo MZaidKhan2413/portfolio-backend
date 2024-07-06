30e72270bb718c0445c3c716bb44f1168ff9beb9
Mohammad Zaid Khan <mzaidk222@gmail.com>
2024-07-06 18:57:16 +0530

    added cors

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
+if (process.env.NODE_ENV != "production") {
+  require("dotenv").config();
+}
+
 const express = require("express");
 const app = express();
 const path = require("path");
 const Contact = require("./models/contact.js");
 const ejsMate = require("ejs-mate");
+const cors = require("cors");
 
 const PORT = process.env.PORT || 3000;
 
@@ -17,10 +22,17 @@ main()
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
+
+app.use(express.json());
+app.use(cors({
+  origin: [process.env.ORIGIN_URL],
+  methods: ['GET', 'POST'],
+  credentials: true,
+}));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({extended: true}));
-app.use(express.json());
 app.use(express.static(path.join(__dirname, "/public")));
 app.engine("ejs", ejsMate);
 
